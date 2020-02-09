ca9c6606fdd1eadae72823b4d52fd9eabe1bc221
Vinay-Thakor <Vstar98@googlemail.com>
2020-02-09 11:00:55 +0000

    My homework 1

--- Division.java
+++ Division.java
@@ -1,8 +1,13 @@
 package homework08022020;
 
 public class Division {
-    public static void main(String[] args){
-        System.out.println(50/3);
+
+    static int x = 50;
+    static int y = 3;
+
+    public static void main(String[] args) {
+        System.out.println((double) x / y);
 
     }
 }
+
